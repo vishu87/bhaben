27bc3b9edd3dac9e0e2e07693b5bfefc0a5c7e93
vishu87 <vishu.iitd@gmail.com>
2018-03-21 21:57:33 +0530

    changes in view

--- site/app/views/admin/members.blade.php
+++ site/app/views/admin/members.blade.php
@@ -1,9 +1,14 @@
 <style type="text/css">
-    tr.item.red {
-        background: #f09696;
+    .barspan {
+        font-size: 11px;
+        color: #000;
+        font-style: italic;
     }
-    tr.item.green {
-        background: #96f09d;
+    .bar.red {
+        background: #d73535;
+    }
+    .bar.green {
+        background: #28d735;
     }
 </style>
 <div style="padding: 10px 0; background: #3080a9">
@@ -23,19 +28,19 @@
 <div style="background:#FFF; padding: 10px 0;min-height: 550px;">
     <div class="container-fluid">
         <h2 class="page-title">{{$jobDetails->job.' (' . $jobDetails->sub_product. ' )'}}</h2>
-        <div>
+        <div  style="overflow-x: auto">
         <table class="table">
             <thead>
                 <tr>
                     <th style="width: 50px;"> SN</th>
                     <th>Name </th>
-                    <th>Seniority (mths) </th>
-                    <th>Months in step</th>
-                    <th style="width: 120px">Individual Global Training Progress</th>
+                    <th style="width: 50px">Seniority (mths) </th>
+                    <th style="width: 50px">Months in step</th>
+                    <th style="width: 250px">Individual Global Training Progress</th>
                     <th style="width: 150px">Individual Expected Global Training Progress</th>
                     <th>% Expected against Actual</th>
-                    <th>Available Fixed Step Months</th>
-                    <th>Months of no training</th>
+                    <th style="width: 100px">Available Fixed Step Months</th>
+                    <th style="width: 100px">Months of no training</th>
                 </tr>
             </thead>
             <tbody>
@@ -55,12 +60,19 @@
 
 
                     ?>
-                    <tr class="item {{$class}}">
+                    <tr class="item">
                         <td> {{$count}}</td>
                         <td> {{$member->full_name}} </td>
                         <td> {{$member->seniority_months}} </td>
                         <td> {{$member->month_step}} </td>
-                        <td> {{$member->indivisual_global_training_progress}} </td>
+                        <td valign="middle" style="overflow: hidden;">
+                            <div style="width: 200px">
+                                @if($member->indivisual_global_training_progress)
+                                <span class="barspan">{{$member->indivisual_global_training_progress}}</span>
+                                <div style="width: {{$member->indivisual_global_training_progress}}%; height: 3px; " class="bar {{$class}}"></div>
+                                @endif
+                            </div>
+                        </td>
                         <td> {{$member->indivisual_expected_global_training_progress}} </td>
                         <td> {{$member->expected_against_actual}} </td>
                         <td> {{$member->available_fixed_step_months}} </td>
@@ -71,7 +83,11 @@
                 @endforeach
             </tbody>
         </table>
-    </div>
+        </div>
+
+        <div class="text-center">
+            <button class="btn blue btn-lg" onclick="alert('Mail has been sent')">Send Email</button>
+        </div>
         
         
     </div>
